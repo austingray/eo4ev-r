c63c8b6a28168b2114ce749fb467daae7d5c3a87
Austin Gray <waustingray@gmail.com>
2016-06-19 00:07:09 -0400

    refactoring routes/views

--- routes/admin.js
+++ routes/admin.js
@@ -14,41 +14,67 @@ var Races = require('../db/races.js');
 var Classes = require('../db/classes.js');
 var Posts = require('../db/posts.js');
 
+/////////////
+// helpers //
+/////////////
+var validateAdmin = function(req, res, callback) {
+  if (req.user && req.user.access === 10) {
+    callback();
+  } else {
+    res.sendStatus(404);
+  }
+}
+
+/////////////////////////////
+// generic response object //
+/////////////////////////////
+var res_object = {
+  title: 'hey there super cool admin guy',
+  admin: true
+}
+
 /////////////////
 // Admin panel //
 /////////////////
 router.get('/', function(req, res, next) {
-  if (req.user) {
-    new Users({ username: req.user.username }).fetch().then(function(model) {
-      if (model === null) res.redirect('/');
-      var user = model.toJSON();
-      if (user.access === 10) {
-        res.render('admin', { title: 'hey there super cool admin guy' });
-      } else {
-        res.sendStatus(404);
-      }
-    });
-  } else {
-    res.sendStatus(404);
-  }
+  validateAdmin(req, res, function() {
+    res_object.section = 'main';
+    res.render('admin', res_object);
+  });
 });
-router.post('/', function(req, res, next) {
-  if (req.user) {
-    new Users({ username: req.user.username }).fetch().then(function(model) {
-      if (model === null) res.redirect('/');
-      var user = model.toJSON();
-      if (user.access === 10) {
-        //passes validation, handle it
-        new Posts({ user_id: req.user.id, post_title: sanitize(req.body.post_title), post_content: sanitize(req.body.post_content) }).save().then(function(model) {
-          res.redirect('/');
-        });
-      } else {
-        res.sendStatus(404);
-      }
+
+///////////
+// posts //
+///////////
+router.get('/post', function(req, res, next) {
+  validateAdmin(req, res, function() {
+    res_object.section = 'post';
+    res.render('admin', res_object);
+  });
+});
+
+router.post('/post', function(req, res, next) {
+  validateAdmin(req, res, function() {
+    new Posts({ user_id: req.user.id, post_title: sanitize(req.body.post_title), post_content: sanitize(req.body.post_content) }).save().then(function(model) {
+      res.redirect('/');
     });
-  } else {
-    res.sendStatus(404);
-  }
+  });
+});
+
+///////////////////
+// asset manager //
+///////////////////
+router.get('/assets', function(req, res, next) {
+  validateAdmin(req, res, function() {
+    res_object.section = 'assets';
+    res.render('admin', res_object);
+  });
+});
+
+router.post('/assets', function(req, res, next) {
+  validateAdmin(req, res, function() {
+    console.log(req.body);
+  });
 });
 
 module.exports = router;
